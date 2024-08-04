3fb17648cadf5e05e535a487bedb44397300e33f
Kartikhub <sharmakartik66@gmail.com>
2024-08-04 20:14:56 +0530

    added spec components and openapi testing spec

---
+++ src/components/Components.tsx
@@ -0,0 +1,13 @@
+
+
+// schemas
+// responses
+// parameters
+// examples
+// requestBodies
+// headers
+// security schemas
+// links
+// callbacks
+// patternProperties
+// additionalProperties
--- src/components/OpenApiEditor.tsx
+++ src/components/OpenApiEditor.tsx
@@ -1,13 +1,19 @@
 import { SideBar } from "./Sidebar"
+// import OpenAPIParser from "@readme/openapi-parser"
+// import petstore from "../assets/petstore.json"
+// import TwoColumnCollapsible from "./two-column-collapsible"
+
 
 export const OpenApiEditor = () => {
+
     return (
         <div className="flex">
-            <div className="w-full">
-                Main Screen
-            </div>
-            <div className="border-l border-[#6D63CD] hidden md:block">
+            <div className="border-r border-[#6D63CD] hidden md:block">
                 <SideBar />
+            </div>
+            <div className="w-full">
+               {/* { api.info.title } */}
+               Main
             </div>
         </div>
     )
